8b07bff651bbf1e2be6b52d7f5c6d1638e1b5cda
Giel <g.cobben@gmail.com>
2024-01-09 08:42:53 +0100

    initial commit

--- app/root.tsx
+++ app/root.tsx
@@ -8,8 +8,10 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import styles from "./tailwind.css";
 
 export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles },
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
@@ -22,7 +24,7 @@ export default function App() {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="bg-white dark:bg-black text-black dark:text-white">
         <Outlet />
         <ScrollRestoration />
         <Scripts />
