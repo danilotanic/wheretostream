a14dc0b22584fa6e46cd9980ad4cdf6467da524b
Giel <g.cobben@gmail.com>
2024-01-10 09:47:18 +0100

    Add tmdb api

--- app/routes/_layout._index.tsx
+++ app/routes/_layout._index.tsx
@@ -1,13 +1,46 @@
-import type { MetaFunction } from "@remix-run/cloudflare";
+import type { LoaderFunctionArgs, MetaFunction } from "@remix-run/cloudflare";
+import { Form, json, useLoaderData } from "@remix-run/react";
+import { useCallback, useRef } from "react";
+import { Input } from "~/components/ui/input";
+
+export async function loader({ context }: LoaderFunctionArgs) {
+  const response = await fetch(`https://api.themoviedb.org/3/movie/upcoming`, {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${context.env.TMDB_ACCESS_TOKEN}`,
+    },
+  });
+  const data = await response.json();
+
+  return json({ data });
+}
 
 export const meta: MetaFunction = () => {
   return [{ title: "Where to stream?" }];
 };
 
 export default function Index() {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const { data } = useLoaderData<typeof loader>();
+
+  console.log("CLIENT: ", data);
+
+  const handleFormClick = useCallback(() => {
+    inputRef.current?.focus();
+  }, []);
+
   return (
-    <div className="wrapper">
-      <h1>Welcome to Whereto.stream</h1>
-    </div>
+    <section className="wrapper">
+      <Form className="flex items-center gap-2 my-20" onClick={handleFormClick}>
+        <h1 className="flex-shrink-0">Where to stream</h1>
+        <Input
+          ref={inputRef}
+          name="search"
+          type="search"
+          placeholder="e.g. the godfather"
+        />
+      </Form>
+    </section>
   );
 }
--- remix.env.d.ts
+++ remix.env.d.ts
@@ -1,3 +1,14 @@
 /// <reference types="@remix-run/dev" />
 /// <reference types="@remix-run/cloudflare" />
 /// <reference types="@cloudflare/workers-types" />
+
+import type { AppLoadContext as OriginalAppLoadContext } from "@remix-run/server-runtime";
+
+declare module "@remix-run/server-runtime" {
+  export interface AppLoadContext extends OriginalAppLoadContext {
+    env: {
+      TMDB_ACCESS_TOKEN: string;
+      TMDB_API_KEY: string;
+    };
+  }
+}
