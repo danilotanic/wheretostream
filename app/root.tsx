95bcaaedd2b29cf14185e8ac1cfe539ce4db47be
Giel <g.cobben@gmail.com>
2024-01-22 17:28:57 +0100

    add nprogress

--- app/root.tsx
+++ app/root.tsx
@@ -7,8 +7,11 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useNavigation,
 } from "@remix-run/react";
 import styles from "./tailwind.css";
+import NProgress from "nprogress";
+import { useEffect, useRef } from "react";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: styles },
@@ -16,6 +19,34 @@ export const links: LinksFunction = () => [
 ];
 
 export default function App() {
+  const navigation = useNavigation();
+  const timer = useRef<number | null>(null); // Type for timer
+
+  // Clear the timer when the component unmounts to prevent memory leaks
+  useEffect(() => {
+    return () => {
+      if (timer.current) clearTimeout(timer.current);
+    };
+  }, []);
+
+  useEffect(() => {
+    // when the state is idle then we can to complete the progress bar
+    if (navigation.state === "idle") {
+      if (timer.current) clearTimeout(timer.current);
+      NProgress.done();
+    }
+    // and when it's something else it means it's either submitting a form or
+    // waiting for the loaders of the next location so we start it
+    else if (!navigation.location?.state?.isSearching) {
+      // Clear any existing timer
+      if (timer.current) clearTimeout(timer.current);
+      // Set a new timer to start the progress bar after 500ms
+      timer.current = setTimeout(() => {
+        NProgress.start();
+      }, 500) as unknown as number; // Delay of 500ms
+    }
+  }, [navigation]);
+
   return (
     <html lang="en">
       <head>
