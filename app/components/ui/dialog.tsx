24e5044402b59d8723099753b73ae5f8c33d3950
Giel <g.cobben@gmail.com>
2024-01-24 13:35:40 +0100

    work

--- app/components/ui/dialog.tsx
+++ app/components/ui/dialog.tsx
@@ -36,7 +36,7 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed left-[50%] top-24 z-50 grid w-full max-w-lg translate-x-[-50%] gap-4 border border-neutral-200 bg-white p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[10%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[10%] sm:rounded-2xl dark:border-neutral-800 dark:bg-neutral-950",
+        "fixed left-[50%] top-6 z-50 grid w-full max-w-lg translate-x-[-50%] gap-4 border border-neutral-200 bg-white p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[10%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[10%] sm:rounded-2xl dark:border-neutral-800 dark:bg-neutral-950",
         className
       )}
       {...props}
